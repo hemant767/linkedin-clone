0f03878be09ea31f8b375878ce372bfa403a099b
hemant767 <hemantatvit2023@gmail.com>
2021-10-05 21:36:06 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './App.css';
 import  Header  from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Login from './Login'
-import {useSelector} from 'react-redux';
-import {selectUser} from './features/userSlice';
+import {useDispatch , useSelector} from 'react-redux';
+import { login ,logout ,selectUser} from './features/userSlice';
+import { auth } from './firebase';
 function App() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        //user is logged in 
+        dispatch(
+          login({
+            email:userAuth.email,
+            uid:userAuth.uid,
+            displayName:userAuth.displayName,
+            photoUrl:userAuth.photoUrl,
+          })
+        );
 
+      } else{
+        // user is loged out
+        dispatch(logout());
+      }
+    });
+  } , []);
   return (
     <div className="App">
       <Header />
@@ -18,10 +39,10 @@ function App() {
         <Login />
       ):(
         <div className="App_body">
-        <Sidebar />
-        <Feed />
+          <Sidebar />
+          <Feed />
 
-      </div>)
+        </div>)
       }
       
     </div>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,65 @@
-import React from 'react'
-import "./Login.css"
-const loginToApp = () => {}
+import React, { useState } from 'react'
+import "./Login.css";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { login } from "./features/userSlice";
+
 const Login = () => {
+    
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email , password)
+        .then(userAuth =>{
+            dispatch(login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: userAuth.user.displayName,
+                photoUrl: userAuth.user.photoURL,
+            }))
+        }).catch((error) => alert(error));
+    };
 
     const register = () => {
+        if (!name) {
+            return alert("Please enter a full name");
 
+        }
+
+        auth.createUserWithEmailAndPassword(email , password)
+        .then((userAuth) =>{
+            userAuth.user.updateProfile({
+                displayname: name,
+                photUrl: profilePic,
+            })
+            .then(() => {
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoUrl: profilePic,
+                }));
+            })
+        }).catch(error => alert(error));
     }
     return (
         <div className="login">
             <img src="https://techcrunch.com/wp-content/uploads/2012/11/linkedin-logo.png" alt="" />
             <form>
-                <input type="text" placeholder="Full Name" />
-                <input type="text" placeholder="Profile picture url(optional)" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Full Name" />
+                <input value={profilePic} onChange = {(e) => setProfilePic(e.target.value)} type="text" placeholder="Profile picture url(optional)" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+                <input value = {password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
                 <button type="submit" onClick={loginToApp}>Sign In</button>
             </form>
 
-            <p>Not a member ?
+            <p>Not a member ?{" "}
                 <span className="login__register" onClick={register}>Register Now</span>
 
             </p>
